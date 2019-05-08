97d4216a2b03a47b380aa9d5f2485c567b8986ba
Rasmus Kröger <rasmuskroger@gmail.com>
2019-05-08 19:12:51 +0300

    Add readme. Force https on prod.

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
+
+const forceSsl = function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+    return next();
+};
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(forceSsl);
+}
+
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
